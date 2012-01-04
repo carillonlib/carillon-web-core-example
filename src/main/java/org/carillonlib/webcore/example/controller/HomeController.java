d6d5d42e8301997f68bdc8582e2830a74eddec05
Eric Montgomery <ericwmontgomery@gmail.com>
2012-01-04 17:26:10 -0500

    adding unlicense public domain declaration

--- src/main/java/org/carillonlib/webcore/example/controller/HomeController.java
+++ src/main/java/org/carillonlib/webcore/example/controller/HomeController.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the Carillon Lib Project.
+ * More information can be found at http://github.com/carillonlib
+ * 
+ * This source code has been released to the public domain.
+ * For more details, see the exact release terms in the
+ * UNLICENSE file, or at http://unlicense.org
+ */
+
 package org.carillonlib.webcore.example.controller;
 
 import org.springframework.stereotype.Controller;
