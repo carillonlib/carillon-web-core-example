d5e1aefde7bd3a7df8f456cdf8bbe920e1e64d18
Eric Montgomery <ericwmontgomery@gmail.com>
2012-01-04 16:33:53 -0500

    implement simple spring webapp

---
+++ src/main/java/org/carillonlib/webcore/example/controller/HomeController.java
@@ -0,0 +1,12 @@
+package org.carillonlib.webcore.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String home() {
+		return "default";
+	}
+}
